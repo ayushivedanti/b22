19f9ed5a732ae2f661423bf62b766d5485ce21a0
Asus <Asus@DESKTOP-DK89G76>
2021-04-06 09:34:21 -0700

    At a time perform 1

---
+++ src/control_statement/Switch.java
@@ -0,0 +1,22 @@
+package control_statement;
+
+public class Switch {
+	public static void main(String[] args) {
+		
+		String input="CD";
+		
+		switch(input) {
+			
+			case"BI":System.out.println("Balence Inquiry");
+			break;
+			case"MS":System.out.println("mini statement");
+			break;
+			case"CD":System.out.println("cash withdrwal");
+			break;
+			case"MT":System.out.println("money transfer");
+			
+		}
+		
+	}
+
+}
